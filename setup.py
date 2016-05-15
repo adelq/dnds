663aad637ee70065d7b14a5037f72c6d69132e0e
Adel Qalieh <aqalieh95@gmail.com>
2016-05-15 15:41:08 -0400

    Add setup.py for inclusion in PyPI

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package = 'dnds'
+version = '1.0'
+
+setup(
+    name=package,
+    version=version,
+    description="Calculate dN/dS ratio precisely (Ka/Ks) using a codon-by-codon counting method.",
+    author="Adel Qalieh",
+    author_email="adelq@sas.upenn.edu",
+    url="https://github.com/adelq/dnds",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ])
