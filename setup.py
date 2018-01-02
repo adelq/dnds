a73479992e164c7cd04b95ca312d25cea4903f40
Adel Qalieh <aqalieh95@gmail.com>
2018-01-02 13:45:59 -0500

    Add Python 3.6 to compatibility

--- setup.py
+++ setup.py
@@ -22,4 +22,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ])
