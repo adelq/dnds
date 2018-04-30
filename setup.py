21772832c36923a57d58d99975092a09cda5a84e
Adel Qalieh <adelq@med.umich.edu>
2018-04-30 11:49:32 -0400

    Update setup.py with long_description and Trove classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 package = 'dnds'
-version = '2.0'
+version = '2.1'
 
 setup(
     name=package,
     version=version,
     description="Calculate dN/dS ratio precisely (Ka/Ks) using a codon-by-codon counting method.",
+    long_description=open("README.rst").read(),
     author="Adel Qalieh",
     author_email="adelq@med.umich.edu",
     url="https://github.com/adelq/dnds",
@@ -15,12 +16,14 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
     ])
